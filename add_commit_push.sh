70e79de53a0cdb55213f5b4d8bb892d68a81a873
HuCaiHui <hucaihuime@foxmail.com>
2019-08-25 16:01:03 +0800

    update

--- add_commit_push.sh
+++ add_commit_push.sh
@@ -5,12 +5,12 @@ git add . &&  echo -e "$set_color git add .     done"
 echo -e "$clear_color"
 if [ $1 ]
 then
-    git commit -m "$1" && echo "$set_color git commit -m \"$1\"  done"
+    git commit -m "$1" && echo -e "$set_color git commit -m \"$1\"  done"
 else
-    git commit -m "update" && echo "$set_color git commit -m \"update\"  done"
+    git commit -m "update" && echo -e "$set_color git commit -m \"update\"  done"
 fi
 echo -e "$clear_color"
-git push origin master && echo "$set_color git push origin master done" 
+git push origin master && echo -e "$set_color git push origin master done" 
 echo -e "$clear_color"
 
 read -p "press anything to continue"var 
